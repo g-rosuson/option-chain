fad73001950fc3c9a28e13289cc62f01638b7d28
GHR <g.rosuson@gmail.com>
2026-02-08 23:43:13 +0100

    chore(frontend): add debugging test

--- src/app/api/exchange-info/route.ts
+++ src/app/api/exchange-info/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from 'next/server';
 
 import config from 'src/config';
 
+export const runtime = 'edge';
+export const preferredRegion = 'fra1';
+
 const GET = async () => {
     try {
         const response = await fetch(config.endpoint.exchangeInfo);
@@ -14,6 +17,8 @@ const GET = async () => {
         }
 
         const data = await response.json();
+
+        console.log("RAW API RESPONSE:", data);
 
         return NextResponse.json(data);
     } catch (error) {
